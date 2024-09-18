e0464dc5f6550b8e920a873d529ec43cb0738f18
JuliusMoehring <44407680+JuliusMoehring@users.noreply.github.com>
2024-09-18 16:08:16 +0200

    Update middleware

--- middleware.ts
+++ middleware.ts
@@ -1,29 +1,5 @@
 export { default } from "next-auth/middleware";
 
-// export const config = {
-//   matcher: [
-//     "/((?!login|static|.*\\..*|_next|api/cache/upload).*)",
-//   ],
-// };
-
-
-// gpt-o1 middleware.ts
-
-import { withAuth } from "next-auth/middleware";
-
-/*
-export default withAuth({
-  callbacks: {
-    authorized: ({ token }) => {
-      // Allow access if the token exists
-      return !!token;
-    },
-  },
-});
-*/ 
-
 export const config = {
-  matcher: [
-    
-  ],
+  matcher: ["/"],
 };
