6d60cb04e1c90180591c805581329cf975146472
JuliusMoehring <44407680+JuliusMoehring@users.noreply.github.com>
2024-09-18 15:56:49 +0200

    Update middleware

--- middleware.ts
+++ middleware.ts
@@ -1,13 +1,5 @@
-import { withAuth } from 'next-auth/middleware';
-import { NextRequest } from 'next/server';
-
-export default function middleware(req: NextRequest) {
-  console.log(`Request URL: ${req.url}`);
-  return withAuth(req);
-}
+export { default } from "next-auth/middleware";
 
 export const config = {
-  matcher: [
-    "/((?!login|static|.*\\..*|_next|api/cache/upload).*)",
-  ],
+  matcher: ["/"],
 };
