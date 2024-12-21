457fd9c2e2490487d9be76dc7e697e7b73a66abe
DirkSchlossmacher <62424946+DirkSchlossmacher@users.noreply.github.com>
2024-12-21 15:09:27 +0100

    o1 no stream

--- app/client/platforms/openai.ts
+++ app/client/platforms/openai.ts
@@ -217,6 +217,7 @@ export class ChatGPTApi implements LLMApi {
 
     const isDalle3 = _isDalle3(options.config.model);
     const isO1 = options.config.model.startsWith("o1");
+    const isO1final = (options.config.model == "o1");
     if (isDalle3) {
       const prompt = getMessageTextContent(
         options.messages.slice(-1)?.pop() as any,
@@ -245,7 +246,7 @@ export class ChatGPTApi implements LLMApi {
       // O1 not support image, tools (plugin in ChatGPTNextWeb) and system, stream, logprobs, temperature, top_p, n, presence_penalty, frequency_penalty yet.
       requestPayload = {
         messages,
-        stream: options.config.stream,
+        stream: !isO1final ? options.config.stream : false,
         model: modelConfig.model,
         temperature: !isO1 ? modelConfig.temperature : 1,
         presence_penalty: !isO1 ? modelConfig.presence_penalty : 0,
